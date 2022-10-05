00f75523fce8984e1737daabb1ed2bc4afa78fe9
Nayoung Park <nyp@nuritelecom.com>
2022-10-05 11:03:40 +0900

    2장 완료

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB1b8nO7JlkygW2XEn4aq_LGjvNzKxjlsg',
+  authDomain: 'nwitter-5d24a.firebaseapp.com',
+  projectId: 'nwitter-5d24a',
+  storageBucket: 'nwitter-5d24a.appspot.com',
+  messagingSenderId: '708556764744',
+  appId: '1:708556764744:web:6854072c948f2ed4540a7d',
+};
+
+export default firebase.initializeApp(firebaseConfig);
